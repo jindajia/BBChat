5abb47fcb9f6c99dd375ff18412ce2c220e28bc6
Cauchyxxx <43872260+Cauchyxxx@users.noreply.github.com>
2022-02-04 16:16:09 -0500

    Add files via upload

---
+++ BackEnd/BBchat/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	routes "BBchat/routes"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	// API-2
+	router.GET("/api-1", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+
+	})
+
+	// API-1
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+}
