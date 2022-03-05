4621fa6fdf1de3c73c6066f0868ec19c27db7d8c
jiajinda <jiajinda@yeah.net>
2022-03-04 20:18:12 -0500

    cypress test

--- FrontEnd/React App/cypress/integration/BBChat_first.spec.js
+++ FrontEnd/React App/cypress/integration/BBChat_first.spec.js
@@ -5,15 +5,47 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 describe('My first test',() =>{
-	it('Visit BBChat website',()=>{
+  	beforeEach(() => {
 		cy.visit('http://localhost:3000')
+	})
+	it('Visit BBChat website',() =>{
 		cy.contains('Login').click()
 	})
 	it('Try Registration',() =>{
-		cy.visit('http://localhost:3000')
 		cy.contains('Registration').click()
 		cy.get('.email').type('jinda').should('have.value','jinda')
 		cy.get('.password').type('jiajinda').should('have.value','jiajinda')
 		cy.contains('RegistrationConfirm').click()
+	})
+    it('check the Sign up button', function(){
+		cy.contains('Registration').click()
+		cy.get('.email').type('jinda').should('have.value','jinda')
+		cy.get('.password').type('jiajinda').should('have.value','jiajinda')
+        cy.contains('Username is not available.')
+        cy.on('window:alert',(txt)=> {
+            //Mocha assertions
+            expect(txt).to.contains('The user already exists');
+        })
+    })
+	it('Try Login', () =>{
+		cy.contains('Login').click()
+		cy.get('.email').type('jinda').should('have.value','jinda')
+		cy.get('.password').type('jiajinda').should('have.value','jiajinda')
+		cy.contains('LoginConfirm').click()
+	    cy.on('window:confirm', () => true);
+	})
+	it('Test Profile', ()=>{
+		cy.contains('Login').click()
+		cy.get('.email').type('jinda').should('have.value','jinda')
+		cy.get('.password').type('jiajinda').should('have.value','jiajinda')
+		cy.contains('LoginConfirm').click()
+		cy.contains('jinda')
+	})
+	it('Test Logout', ()=>{
+		cy.contains('Login').click()
+		cy.get('.email').type('jinda').should('have.value','jinda')
+		cy.get('.password').type('jiajinda').should('have.value','jiajinda')
+		cy.contains('LoginConfirm').click()
+		cy.contains('Logout').click()
 	})
 })
