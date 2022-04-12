2b7b18115dbc788e5046c1bd06f3b211ced41280
superoctober1213 <56080068+superoctober1213@users.noreply.github.com>
2022-04-12 12:07:47 -0400

    mainhome4.0-add clock

--- FrontEnd/React App/src/pages/mainhome/mainhome.js
+++ FrontEnd/React App/src/pages/mainhome/mainhome.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { withRouter } from 'react-router-dom';
+import moment from 'moment'
 
 import './mainhome.css'
+
+function FormattedDate(props) {
+    return (
+        <div>
+            <h2 className='year'>{moment().format('YYYY-MM-DD')}</h2>
+            <h1 className='hour'>{moment().format('HH:mm:ss')}</h1>
+        </div>
+    );
+}
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timeID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timeID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <FormattedDate date={this.state.date} />
+            </div>
+        );
+    }
+}
 
 function Mainhome() {
     return (
@@ -22,10 +61,19 @@ function Mainhome() {
                 </div>
             </div>
 
+            <div className='tp-cl-container'>
+                <div className='clock-container'>
+                    <Clock />
+                </div>
+                <div className='tp-container'>
+
+                </div>
+            </div>
+
             <div class="content-3d">
                 <button className='btn-3d green'>Hot Topic</button>
                 <button className='btn-3d purple'>New Meeting</button>
-                <button className='btn-3d cyan'>Join</button>
+                <button className='btn-3d cyan'>Join Meeting</button>
                 <button className='btn-3d yellow'>Friends</button>
             </div>
 
