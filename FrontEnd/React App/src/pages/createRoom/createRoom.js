396e64d19b508d5034f40d75c4642369ee51cbb9
superoctober1213 <56080068+superoctober1213@users.noreply.github.com>
2022-04-18 00:13:27 -0400

    createroom-button

--- FrontEnd/React App/src/pages/createRoom/createRoom.js
+++ FrontEnd/React App/src/pages/createRoom/createRoom.js
@@ -149,58 +149,12 @@ function CreateRoom(props) {
         </div>
 
         <div className='create_button'>
-          <button className='button-style2' onClick={createRoom}>CreateRoom</button>
+          <button className='button-49' onClick={createRoom}>CreateRoom</button>
         </div>
       </div>
     </div>
   );
 }
 
-// const docStyle = document.documentElement.style
-// const aElem = document.querySelector('button')
-// const boundingClientRect = aElem.getBoundingClientRect()
-
-// aElem.onmousemove = function(e) {
-
-// 	const x = e.clientX - boundingClientRect.left
-// 	const y = e.clientY - boundingClientRect.top
-
-// 	const xc = boundingClientRect.width/2
-// 	const yc = boundingClientRect.height/2
-
-// 	const dx = x - xc
-// 	const dy = y - yc
-
-// 	docStyle.setProperty('--rx', `${ dy/-1 }deg`)
-// 	docStyle.setProperty('--ry', `${ dx/10 }deg`)
-
-// }
-
-// aElem.onmouseleave = function(e) {
-
-// 	docStyle.setProperty('--ty', '0')
-// 	docStyle.setProperty('--rx', '0')
-// 	docStyle.setProperty('--ry', '0')
-
-// }
-
-// aElem.onmousedown = function(e) {
-
-// 	docStyle.setProperty('--tz', '-25px')
-
-// }
-
-// document.body.onmouseup = function(e) {
-
-// 	docStyle.setProperty('--tz', '-12px')
-
-// }
-
-
-
-// ReactDOM.render(
-//   <CreateRoom />,
-//   document.getElementById('root')
-// );
 
 export default withRouter(CreateRoom);
