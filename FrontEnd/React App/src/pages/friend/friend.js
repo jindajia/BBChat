a76354dccc9613a0bdd8527e0d1024583bb28406
superoctober1213 <56080068+superoctober1213@users.noreply.github.com>
2022-04-17 22:48:10 -0400

    friend2.0

--- FrontEnd/React App/src/pages/friend/friend.js
+++ FrontEnd/React App/src/pages/friend/friend.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Link, withRouter } from 'react-router-dom';
+import { Tooltip, Whisper } from 'rsuite';
+import { Button, IconButton, ButtonGroup, ButtonToolbar } from 'rsuite';
+import { Input, InputGroup, MaskedInput } from 'rsuite';
+
+import "./friend.css"
+
 
 function Navhome() {
 
@@ -29,14 +35,47 @@ function Navhome() {
     );
 }
 
+const tooltip1 = (
+    <Tooltip className='tip'>
+        Chat with a friend
+    </Tooltip>
+);
+
+const tooltip2 = (
+    <Tooltip className='tip'>
+        Chat with a random online user
+    </Tooltip>
+);
+
 
 function Friend() {
     return (
-    <div>
         <div>
-            <Navhome />
+            <div>
+                <Navhome />
+            </div>
+            <div >
+                <div className='add-container'>
+                    <Whisper trigger="focus" speaker={<Tooltip className='tip'>Friend's Username</Tooltip>}>
+                        <Input className='add-input' style={{ width: 280, height: 40 }} placeholder="Default Input" />
+                    </Whisper>
+                </div>
+                <div className='add-button'>
+                    <Whisper placement='right' controlId="control-id-hover" trigger="hover" speaker={tooltip1}>
+                        <Button className='button-85'>Add Friend</Button>
+                    </Whisper>
+                </div>
+
+            </div>
+
+            <div className='random-container'>
+                <Whisper placement='right' controlId="control-id-hover" trigger="hover" speaker={tooltip2}>
+                    <Button className='button-85'>Random Chat</Button>
+                </Whisper>
+
+            </div>
+
         </div>
-    </div>
     );
 }
 
