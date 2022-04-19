53e1368c3eba20861f1969c0dbafa70954d8070d
jiajinda <jiajinda@yeah.net>
2022-04-19 15:32:52 -0400

    join meeting page

--- FrontEnd/React App/src/pages/joinmeeting/joinmeeting.js
+++ FrontEnd/React App/src/pages/joinmeeting/joinmeeting.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Link, withRouter } from 'react-router-dom';
 import {getItemInLS} from "./../../services/storage-service";
+import { userJoinRoom } from "./../../services/api-service";
 
 
 import "./joinmeeting.css"
+import {ReactNotifications, Store } from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
 
 const getUserName = (userDetails) => {
     if (userDetails && userDetails.username) {
@@ -36,23 +39,96 @@ function Navhome() {
     );
 }
 
-function JoinMeeting() {
+function JoinMeeting(props) {
+
+    const [roomNumber, updateRoomNumber] = useState("");
+    const [roomPassword, updateRoomPassword] = useState("");
+    const userDetails = getItemInLS('userDetails');
+    const handleroomNumberChange = (event) => {
+        updateRoomNumber(event.target.value);
+      }
+    
+    const handleRoomPasswordChange = (event) => {
+        updateRoomPassword(event.target.value);
+    }
+    const joinRoom = async () => {
+        // props.displayPageLoader(true);
+        const joinRoomMessage = {}
+        joinRoomMessage.username = userDetails.username;
+        joinRoomMessage.roomNo = roomNumber;
+        joinRoomMessage.roomPassword = roomPassword;
+        console.log(joinRoomMessage);
+        var joinResponse = null;
+        try{
+            joinResponse = await userJoinRoom(joinRoomMessage);
+        } catch(err) {
+          console.log(err);
+          Store.addNotification({
+            title: "Join Room Failed!",
+            message: "connection error",
+            type: "danger",
+            insert: "top",
+            container: "top-right",
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],
+            dismiss: {
+              duration: 5000,
+              onScreen: true
+            }
+          });
+        }
+        // props.displayPageLoader(false);
+    
+        if (joinResponse===200){
+            console.log(joinResponse.response)
+            Store.addNotification({
+              title: "Join Room Success!",
+              message: joinResponse.response,
+              type: "success",
+              insert: "top",
+              container: "top-right",
+              animationIn: ["animate__animated", "animate__fadeIn"],
+              animationOut: ["animate__animated", "animate__fadeOut"],
+              dismiss: {
+                duration: 5000,
+                onScreen: true
+              }
+            });
+        } else {
+          console.log(joinResponse.response)
+          Store.addNotification({
+            title: "Join Room Failed!",
+            message: joinResponse.response,
+            type: "danger",
+            insert: "top",
+            container: "top-right",
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],
+            dismiss: {
+              duration: 5000,
+              onScreen: true
+            }
+          });
+        }
+    };
+
     return (
         <div>
             <div>
                 <Navhome />
             </div>
             <div>
+                <ReactNotifications />
                 <div className='number-container'>
                     <label className='label-style'>RoomNumber:</label>
-                    <input className='input-style' />
+                    <input className='input-style' value={roomNumber} onChange={handleroomNumberChange} />
                 </div>
                 <div className='password-container'>
                     <label className='label-style'>Roompassword:</label>
-                    <input className='input-style' />
+                    <input className='input-style' value={roomPassword} onChange={handleRoomPasswordChange}/>
                 </div>
                 <div className='button-container'>
-                    <button className='button-73'>JOIN</button>
+                    <button className='button-73' onClick={joinRoom}>JOIN</button>
                 </div>
             </div>
         </div>
